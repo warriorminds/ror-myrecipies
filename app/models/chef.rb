1e23cc99dc89bca817423899b5f8f0c5353ad86b
= <=>
2017-04-05 20:39:17 +0000

    Edit actions

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,7 +5,7 @@ class Chef < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true # only allow nil in updates, not on create.
   
   has_many :recipes
   has_secure_password
