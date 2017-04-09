b3d1d0e93db4ec396265378d7b50b2598863a88a
= <=>
2017-04-09 18:59:29 +0000

    make app production ready with action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://warriorminds-myrecipes.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://warriorminds-myrecipes.herokuapp.com', 'http://warriorminds-myrecipes.herokuapp.com']
 end
